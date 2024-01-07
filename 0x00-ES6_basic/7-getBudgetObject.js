fdf047d909e118284a5288014cfc5b2b9445b0b1
ActuaryEmma <emmanyakio@gmail.com>
2024-01-07 11:37:11 +0300

    Task 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,13 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    income: income,
-    gdp: gdp,
-    capita: capita,
+    income,
+    gdp,
+    capita,
   };
-  const keyname = {}
-  for (const name in budget){
-	  keyname[name] = budget[name]
-  }
 
-  return keyname;
+  return budget;
 }
