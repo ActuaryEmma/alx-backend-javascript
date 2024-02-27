9d5c4f5f0d608c96658ea5c28362b6f68950aea4
ActuaryEmma <emmanyakio@gmail.com>
2024-02-28 01:13:57 +0300

    Task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * Define a function that prints a given
+ * argument to the standard output stream.
+ *
+ * */
+function displayMessage(message) {
+	console.log(message)
+}
+module.exports = displayMessage;
