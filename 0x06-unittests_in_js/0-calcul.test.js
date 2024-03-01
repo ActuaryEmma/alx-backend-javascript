7b01b536b5c1017a292585d64077a2e4822a7ec2
ActuaryEmma <emmanyakio@gmail.com>
2024-03-01 09:58:53 +0300

    Task 0 update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,6 +3,8 @@ const calculateNumber = require("./0-calcul.js");
 
 describe("calculateNumber(a, b)", function() {
 	it("return sum of rounded numbers", function() {
+		a = Math.round(a)
+		b = Math.round(b)
 		const result = calculateNumber(1.5, 2.7)
 		assert.equal(result, 5);
 	});
