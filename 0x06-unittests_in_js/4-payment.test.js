977fb9361acdf219c7f3a0e70888cb3b4c92fa65
ActuaryEmma <emmanyakio@gmail.com>
2024-03-01 23:10:22 +0300

    Task 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,20 +1,16 @@
-const sinon = require('sinon');
-const Utils = require('./utils');
 const { expect } = require('chai');
+const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
 
-describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
-    const bigBrother = sinon.spy(console);
-    const dummy = sinon.stub(Utils, 'calculateNumber');
-
-    dummy.returns(10);
+describe('sendPaymentRequestToApi', function () {
+  it('sendPaymentRequestToApi(100, 20) returns The total is: 10', function () {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const spy = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
-    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(dummy.callCount).to.be.equal(1);
-    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
-    expect(bigBrother.log.callCount).to.be.equal(1);
-    dummy.restore();
-    bigBrother.log.restore();
+    expect(stub.firstCall.args).to.eql(['SUM', 100, 20]);
+    expect(spy.calledWith('The total is: 10')).to.be.true;
+    spy.restore();
+    stub.restore();
   });
 });
