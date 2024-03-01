93bba49a27c47d33adfa96125cd398eef731aa01
ActuaryEmma <emmanyakio@gmail.com>
2024-03-01 10:10:23 +0300

    added more test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,20 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul.js");
 
 describe("calculateNumber(a, b)", function() {
-	it("return sum of rounded numbers", function() {
-		a = Math.round(a)
-		b = Math.round(b)
-		const result = calculateNumber(1.5, 2.7)
+	it("return sum", function() {
+		const result = calculateNumber(3, 2)
 		assert.equal(result, 5);
+	});
+	it("round of first number", function() {
+		const result = calculateNumber(3.2, 2)
+		assert.equal(result, 5)
+	});
+	it("round off second number", function() {
+		const result = calculateNumber(3, 2.2)
+		assert.equal(result, 5)
+	});
+	it("return sum of rounded numbers", function() {
+		const result = calculateNumber(3.2, 2.2)
+		assert.equal(result, 5)
 	});
 });
